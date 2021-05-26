716f72206b36a8b0176757b9e62f35cf42a16154
anirudh2810 <anirudhchopra40@gmail.com>
2021-05-26 15:28:28 +0530

    code refactored

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import react from 'react';
-import ReactDom from 'react-dom';
-import App from './components/App';
-import 'semantic-ui-css/semantic.min.css';
-import './components/styles.css';
-
-ReactDom.render(<App />, document.querySelector('#root'));
